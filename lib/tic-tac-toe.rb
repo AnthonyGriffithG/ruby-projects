1cdae333ce5b6ffe176b138f43d406c86b49edc7
Anthony Griffith <a.griffith.gonzalez@gmail.com>
2024-06-04 11:28:51 -0600

    improve tic tac toe adding a third class

--- lib/tic-tac-toe.rb
+++ lib/tic-tac-toe.rb
@@ -1,8 +1,17 @@
+class Player
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+end
+
 class Board
   attr_reader :last_player_won, :previous_moves, :available_moves
 
   def initialize
-    @available_moves = [1,2,3,4,5,6,7,8,9]
+    @available_moves = (1..9).to_a
     @previous_moves = []
     @last_player_won = false
   end
@@ -19,10 +28,10 @@ class Board
     ]
 
     first_player_moved_last = @previous_moves.length.even?
-      last_player_moves = @previous_moves.filter do | play |
-        index = @previous_moves.find_index play
-        first_player_moved_last ? index.odd? : index.even?
-      end
+    last_player_moves = @previous_moves.filter do |play|
+      index = @previous_moves.find_index(play)
+      first_player_moved_last ? index.odd? : index.even?
+    end
 
     winning_patterns.each do |pattern|
       if (pattern - last_player_moves).empty?
@@ -52,34 +61,42 @@ end
 
 class Game
   def initialize(player_1_name, player_2_name)
-    @player_1 = player_1_name
-    @player_2 = player_2_name
+    @player_1 = Player.new(player_1_name, 'X')
+    @player_2 = Player.new(player_2_name, 'O')
     @board = Board.new
     start
   end
 
 
   def start
+    current_player = @player_1
+
     until @board.last_player_won || @board.available_moves.length == 0
-      @board.previous_moves.length.even? ? p('Player 1 turn') : p('Player 2 turn')
+      p "#{current_player.name}'s turn (#{current_player.symbol})"
       play = gets.chomp.to_i
       if @board.available_moves.include? play
         @board.make_move play
         @board.display_board
         @board.is_winning_move
+        current_player = next_player(current_player)
       else
         p "Invalid move, the available moves are #{@board.available_moves.to_s}"
       end
     end
 
     if @board.last_player_won
-      winner = @board.previous_moves.length.odd? ? @player_1 : @player_2
+      winner = current_player == @player_1 ? @player_2.name : @player_1.name
       p "The winner is: #{winner}!"
     else
       p "The game ends in a draw!"
     end
   end
 
+  private
+
+  def next_player(current_player)
+    current_player == @player_1 ? @player_2 : @player_1
+  end
 end
 
 
